7c0af718a78bba6ec21cd5474d4402de0f385234
ardhisr <radika.kerenz1@gmail.com>
2021-04-30 22:42:25 +0700

    done get data politicians from database

--- Data/DBHelper.go
+++ Data/DBHelper.go
@@ -25,14 +25,13 @@ func connect() (*sql.DB, error) {
 	return db, nil
 }
 
-func getDataPoliticians() {
+func GetDataPoliticians() (dataPoliticiansDB []PoliticiansDB) {
 	db, err := connect()
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
 
-	var dataPoliticiansDB []PoliticiansDB
 	data, err := db.Query("SELECT * FROM politicians")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -54,9 +53,10 @@ func getDataPoliticians() {
 
 		dataPoliticiansDB = append(dataPoliticiansDB, dat)
 	}
-	//return dataPoliticiansDB
+	return
 }
 
+//this function for initial post from json only do not use for others
 func InitialPostDataPoliticians(id int, name string, party string, location string, grade float32) {
 	db, err := connect()
 	if err != nil {
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"latihan-web-service-go/Data"
+)
+
 func main() {
+	//initial get datafrom json to database, just activate the function in beginning
 	//Data.PoliticianData()
+
+	datas := Data.GetDataPoliticians()
+	for _, data := range datas {
+		fmt.Println(data.Politician_id)
+		fmt.Println(data.Name)
+		fmt.Println(data.Party)
+		fmt.Println(data.Location)
+		fmt.Println(data.Grade_current)
+	}
 }
