25008bf10f8dfa70b4fd0c4d745be7af3be52d45
marwanjuna <mrwnolii@gmail.com>
2021-04-30 21:47:53 +0700

    release 2 poin 3 done

--- main.go
+++ main.go
@@ -218,6 +218,58 @@ func voterMaleQuery() {
 	}
 }
 
+func popularPoliticianNY() {
+	db, err := connect()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	rows, err := db.Query(
+		`SELECT politician_id, name, party, location, grade_current, MAX(total) total
+		FROM (SELECT p.politician_id, p.name, p.party, p.location, p.grade_current,
+					 COUNT(v.politician_id) as total
+		FROM politicians AS p
+		JOIN votings AS v ON p.politician_id = v.politician_id
+		WHERE p.location = 'NY'
+		GROUP BY p.politician_id) cp`,
+	)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer rows.Close()
+
+	var p []PoliticianWithTotalVotes
+
+	for rows.Next() {
+		var each = PoliticianWithTotalVotes{}
+		var err = rows.Scan(&each.PoliticianID, &each.Name, &each.Party, &each.Location, &each.GradeCurrent, &each.TotalVotes)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		p = append(p, each)
+	}
+
+	if err = rows.Err(); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	for _, each := range p {
+		fmt.Println("Politician ID:", each.PoliticianID)
+		fmt.Println("Name:", each.Name)
+		fmt.Println("Party:", each.Party)
+		fmt.Println("Location:", each.Location)
+		fmt.Println("Grade Current:", each.GradeCurrent)
+		fmt.Println("Total Votes:", each.TotalVotes)
+	}
+}
+
 func main() {
 	// p := decodePoliticianData("politicians.json")
 	// v := decodeVotingData("voting.json")
@@ -226,5 +278,6 @@ func main() {
 	// insertVotingDataToDb(v)
 
 	// allPoliticianQuery()
-	voterMaleQuery()
+	// voterMaleQuery()
+	popularPoliticianNY()
 }
