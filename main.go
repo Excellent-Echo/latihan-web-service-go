390ea732c26f0067082d522d2dfbe3b6426dd0f5
marwanjuna <mrwnolii@gmail.com>
2021-04-30 12:45:01 +0700

    decode voting.json succeed

--- main.go
+++ main.go
@@ -17,7 +17,18 @@ type Politician struct {
 
 type Politicians []Politician
 
-func main() {
+type Voting struct {
+	VoterID      int    `json:"voter_id"`
+	PoliticianID int    `json:"politician_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Gender       string `json:"gender"`
+	Age          int    `json:"age"`
+}
+
+type Votes []Voting
+
+func getPoliticianData() {
 	var politicians Politicians
 
 	jsonFile, err := os.Open("politicians.json")
@@ -25,8 +36,6 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
-
-	fmt.Println("Success open jsonFile")
 
 	defer jsonFile.Close()
 
@@ -37,5 +46,44 @@ func main() {
 		return
 	}
 
-	fmt.Println(politicians)
+	for _, val := range politicians {
+		fmt.Println("Politician ID: ", val.PoliticianID)
+		fmt.Println("Name: ", val.Name)
+		fmt.Println("Party: ", val.Party)
+		fmt.Println("Location: ", val.Location)
+		fmt.Println("Grade Current: ", val.GradeCurrent)
+	}
+}
+
+func getVotingData() {
+	var votes Votes
+
+	jsonFile, err := os.Open("voting.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	err = json.Unmarshal(byteValue, &votes)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	for _, val := range votes {
+		fmt.Println("Voter ID: ", val.VoterID)
+		fmt.Println("Politician ID: ", val.PoliticianID)
+		fmt.Println("First Name: ", val.FirstName)
+		fmt.Println("Last Name: ", val.LastName)
+		fmt.Println("Gender: ", val.Gender)
+		fmt.Println("Age: ", val.Age)
+	}
+}
+
+func main() {
+	getPoliticianData()
+	getVotingData()
 }
