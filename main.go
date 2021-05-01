439c9a0d080999311b5fa06ba702b4f7f685450f
ardhisr <radika.kerenz1@gmail.com>
2021-05-01 21:51:54 +0700

    done release 3 task 1 dan 4

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"html/template"
+	"latihan-web-service-go/Data"
+	"net/http"
+)
+
 func main() {
 	//initial get data from json to database, just activate the function in beginning
 	//Data.PoliticianData()
@@ -12,7 +19,7 @@ func main() {
 	//datas := Data.GetDataPoliticianById(1)
 
 	//Get Data Male Voter with Bname
-	// datas := Data.GetDataMaleVotersWithBname()
+	// datas := Data.GetDataMaleVotersWithBname()git
 
 	// Get Politican with highest score on NY
 	//datas := Data.GetDataPoliticianWithHighestScoreOnNY()
@@ -20,4 +27,47 @@ func main() {
 	// Get 3 Politicians highest Score
 	//datas := Data.GetDataPoliticiansHeadWithHighestScore()
 
+	http.HandleFunc("/votings", func(rw http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("viewVoting.html")
+
+		datas := Data.GetDataVoters()
+		// for _, data := range datas {
+		// 	fmt.Println(data.Voter_id)
+		// 	fmt.Println(data.Politician_id)
+		// 	fmt.Println(data.First_name)
+		// 	fmt.Println(data.Last_name)
+		// 	fmt.Println(data.Gender)
+		// 	fmt.Println(data.Age)
+		// }
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(rw, datas)
+	})
+
+	http.HandleFunc("/politicians", func(rw http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("viewVoting.html")
+
+		datas := Data.GetDataPoliticians()
+		// for _, data := range datas {
+		// 	fmt.Println(data.Voter_id)
+		// 	fmt.Println(data.Politician_id)
+		// 	fmt.Println(data.First_name)
+		// 	fmt.Println(data.Last_name)
+		// 	fmt.Println(data.Gender)
+		// 	fmt.Println(data.Age)
+		// }
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(rw, datas)
+	})
+
+	port := "localhost:8080"
+	fmt.Println("Server Running on port :", port)
+	http.ListenAndServe(port, nil)
 }
