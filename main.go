77cca0b7f72863667680e81b3b42f428a51820b2
Ah Rifqi Ubaidilah <42555735+rifqi23ahmad@users.noreply.github.com>
2021-05-03 00:38:12 +0700

    data masih belum tampil mas

--- main.go
+++ main.go
@@ -15,18 +15,18 @@ import (
 // "location" : "IL",
 // "grade_current"
 type PolitianData struct {
-	Politik []Data `json`
+	Politik []Data `json:"politik"`
 }
 type Data struct {
-	Politian_id   string `json : "politician_id"`
-	Name          string `json : "name"`
-	Party         string `json : "party"`
-	Location      string `json : "location"`
-	Grade_current int    `json : "grade_current"`
+	Politian_id   int    `json:"politician_id"`
+	Name          string `json:"name"`
+	Party         string `json:"party"`
+	Location      string `json:"location"`
+	Grade_current int    `json:"grade_current"`
 }
 
 func main() {
-	jsonFile, err := os.Open("dataJson/politicians.json")
+	jsonFile, err := os.Open("politicians.json")
 
 	if err != nil {
 		fmt.Println(err.Error())
@@ -39,17 +39,17 @@ func main() {
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-	var Politik PolitianData
+	var politik PolitianData
 
-	json.Unmarshal(byteValue, &Politik)
-	fmt.Println("Politik location: " + Politik.Politik[0].Location)
+	json.Unmarshal([]byte(byteValue), &politik)
+	// fmt.Println(byteValue)
 
-	for i := 0; i < len(Politik.Politik); i++ {
-		fmt.Println("Politik politian_id: " + Politik.Politik[i].Politian_id)
-		fmt.Println("Politik name: " + Politik.Politik[i].Name)
-		fmt.Println("Politik party: " + Politik.Politik[i].Party)
-
-		fmt.Println("Politik grade_current: " + strconv.Itoa(Politik.Politik[i].Grade_current))
+	for i := 0; i < len(politik.Politik); i++ {
+		fmt.Println("Politik politian_id: " + strconv.Itoa(politik.Politik[i].Politian_id))
+		fmt.Println("Politik name: " + politik.Politik[i].Name)
+		fmt.Println("Politik party: " + politik.Politik[i].Party)
+		fmt.Println("Politik location: " + politik.Politik[i].Location)
+		fmt.Println("Politik grade_current: " + strconv.Itoa(politik.Politik[i].Grade_current))
 
 	}
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
