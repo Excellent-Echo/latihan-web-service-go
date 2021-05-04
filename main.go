6e7506b7a3a388bced586f60442214e5b9893199
ammarteuku05 <ammarteuku09@gmail.com>
2021-05-04 14:14:10 +0700

    release 3 no 2 selesai

--- main.go
+++ main.go
@@ -6,29 +6,9 @@ import (
 	"webservice/routing"
 )
 
-// type Politician struct {
-// 	PoliticianID   int     `json:"politician_id"`
-// 	NamePolitician string  `json:"name"`
-// 	Party          string  `json:"party"`
-// 	Location       string  `json:"location"`
-// 	GradeCurrent   float32 `json:"grade_current"`
-// }
-
-// type Politicians []Politician
-
-// type Voting struct {
-// 	VotingID     int    `json:"voter_id"`
-// 	PoliticianID int    `json:"policitian_id"`
-// 	FirstName    string `json:"first_name"`
-// 	LastName     string `json:"last_name"`
-// 	Gender       string `json:"gender"`
-// 	Age          int    `json:"age"`
-// }
-
-// type VotingData []Voting
-
 func main() {
 	http.HandleFunc("/voting", routing.ShowVoting)
+	http.HandleFunc("/votingbymale", routing.ShowVotingbyMale)
 
 	port := "localhost:3000"
 
--- routing/routing.go
+++ routing/routing.go
@@ -51,3 +51,46 @@ func ShowVoting(w http.ResponseWriter, r *http.Request) {
 	// kalau bukan method get tampilkan error
 	http.Error(w, "error method not GET", http.StatusBadRequest)
 }
+
+func ShowVotingbyMale(w http.ResponseWriter, r *http.Request) {
+	db := config.Connect()
+
+	dataSQL, err := db.Query("select * from  voting where gender='male'")
+
+	if err != nil {
+		fmt.Println("error query SQL")
+		return
+	}
+
+	var datas []entity.Voting
+
+	for dataSQL.Next() {
+		var each = entity.Voting{}
+
+		if err := dataSQL.Scan(&each.VotingID, &each.PoliticianID, &each.FirstName, &each.LastName, &each.Gender, &each.Age); err != nil {
+			log.Fatal(err.Error())
+			return
+		}
+
+		datas = append(datas, each)
+	}
+	defer db.Close()
+
+	r.Header.Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		// excecute yg bener
+		byteDatas, err := json.Marshal(datas)
+
+		if err != nil {
+			http.Error(w, "error internal server", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(byteDatas)
+		return
+	}
+
+	// kalau bukan method get tampilkan error
+	http.Error(w, "error method not GET", http.StatusBadRequest)
+}
